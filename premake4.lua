acefba06ba10c5958c16bd1b72bd8640e5a58add
Gary Sangwell <garylj@sangwell.co.uk>
2015-04-17 14:58:46 +0100

    Added premake4.lua

---
+++ premake4.lua
@@ -0,0 +1,57 @@
+solution "Games-Programming-CGP2011M"
+	configurations { "Debug", "Release" }
+
+	configuration { "Debug" }
+		targetdir "bin/Debug"
+		defines { "DEBUG" }
+		flags { "Symbols" }
+
+	configuration { "Release" }
+		targetdir "bin/Release"
+		defines { "NDEBUG" }
+		flags { "Optimize" }
+
+	configuration { "windows" }
+		defines { "WINDOWS" }
+	configuration { "linux" }
+		defines { "LINUX" }
+	configuration {}
+	
+	configuration "windows"
+		links { "SDL/SDL2", "SDL/SDL2main", "opengl32", "GLEW/glew32" }
+	configuration "linux"
+		links { "SDL2", "SDL2main", "GL", "GLEW" }
+	configuration {}
+
+	project "Engine"
+		language "C++"
+		kind "StaticLib"
+
+		-- C++ on linux
+		configuration { "linux" }
+			buildoptions "-std=c++11"
+		configuration {}
+
+		files { "./Engine/**.h", "./Engine/**.cpp" }
+		excludes { "./includes/**", "./libraries/**" }
+		
+		includedirs { "./includes/" }
+		libdirs { "./libraries/" }
+		
+	project "ZombieSurvival"
+		language "C++"
+		kind "ConsoleApp"
+
+		-- C++ on linux
+		configuration { "linux" }
+			buildoptions "-std=c++11"
+		configuration {}
+
+		files { "./ZombieSurvival/**.h", "./ZombieSurvival/**.cpp" }
+		excludes { "./includes/**", "./libraries/**" }
+
+		includedirs { "./includes/", "./Engine/" }
+		libdirs { "./libraries/" }
+		
+		-- Link to Engine static library
+		links { "Engine" }
