fd85c4862ab26cb42cac86fd3dfe740f3e4941b0
Gary Sangwell <garylj@sangwell.co.uk>
2015-04-22 05:54:27 +0100

    Added Time system.
    Added system to keep track of delta timing.

---
+++ Engine/Core/Time.h
@@ -0,0 +1,28 @@
+#ifndef _TIME_H
+#define _TIME_H
+
+#include "../Engine/EntitySystem/System.h"
+
+namespace Engine
+{
+	class Time : public EntitySystem::System<Time>
+	{
+	public:
+		virtual void initialise();
+		
+		void update();
+		void updateFixed();
+
+		float getDeltaTime();
+
+	private:
+		float deltaTime = 0.0f;
+		unsigned int lastTime = 0;
+
+		// TODO: fixed timestepping for Physics
+		//float fixedDeltaTime = 0.0f;
+
+	};
+}
+
+#endif
