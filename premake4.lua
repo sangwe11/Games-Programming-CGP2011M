a86150c5054cb2b4e2b2c7b98926a3735a8d54e5
Gary Sangwell <garylj@sangwell.co.uk>
2015-04-24 16:25:49 +0100

    Added missing dependanices.
    Added dependancies .gitignore failed to include.
    Also added post build commands to premake4.lua to make it easier for the marker to compile and run.

--- premake4.lua
+++ premake4.lua
@@ -59,5 +59,16 @@ solution "Games-Programming-CGP2011M"
 		configuration "windows"
 			links { "SDL/SDL2", "SDL/SDL2main", "SDL/SDL2_image", "ASSIMP/assimp", "opengl32", "GLEW/glew32", "freetype/freetype", "irrKlang-1.5.0/irrKlang" }
 		configuration "linux"
-			links { "SDL2", "SDL2main", "SDL2_image", "assimp", "GL", "GLEW", "freetype" }
+			links { "SDL2", "SDL2main", "SDL2_image", "assimp", "GL", "GLEW", "freetype", "irrKlang" }
+		configuration {}
+		
+		-- Post build commands
+		configuration { "Debug", "windows" }
+			postbuildcommands { "xcopy models bin\\Debug\\models /s /e /h /i /Y", "xcopy shaders bin\\Debug\\shaders /s /e /h /i /Y", "xcopy textures bin\\Debug\\textures /s /e /h /i /Y", "xcopy sounds bin\\Debug\\sounds /s /e /h /i /Y", "xcopy fonts bin\\Debug\\fonts /s /e /h /i /Y", "xcopy windows\\dlls bin\\Debug /s /e /h /i /Y" }
+		configuration { "Release", "windows" }
+			postbuildcommands { "xcopy models bin\\Release\\models /s /e /h /i /Y", "xcopy shaders bin\\Release\\shaders /s /e /h /i /Y", "xcopy textures bin\\Release\\textures /s /e /h /i /Y", "xcopy sounds bin\\Release\\sounds /s /e /h /i /Y", "xcopy fonts bin\\Release\\fonts /s /e /h /i /Y", "xcopy windows\\dlls bin\\Release /s /e /h /i /Y" }
+		configuration { "Debug", "linux" }
+			postbuildcommands { "cp models bin/Debug/models -r", "cp shaders bin/Debug/shaders -r", "cp textures bin/Debug/textures -r", "cp sounds bin/Debug/sounds -r", "cp fonts bin/Debug/fonts -r" }
+		configuration { "Release", "linux" }
+			postbuildcommands { "cp models bin/Release/models -r", "cp shaders bin/Release/shaders -r", "cp textures bin/Release/textures -r", "cp sounds bin/Release/sounds -r", "cp fonts bin/Release/fonts -r" }
 		configuration {}
